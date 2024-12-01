af4b44793d36240cd8dd2f7da97ca09b8e1b724f
Ahmet Koçyiğit <kocyigittahmet@gmail.com>
2024-12-01 23:46:27 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string District { get; set; }
+        public string City{ get; set; }
+    }
+}
